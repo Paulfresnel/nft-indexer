c13effc535c9ef834907164d2dac80b75855cfc1
Paulfresnel <paul.fresnel1@gmail.com>
2023-06-25 19:39:10 +0200

    Styling and Features finished

--- src/App.jsx
+++ src/App.jsx
@@ -8,42 +8,56 @@ import {
   Input,
   SimpleGrid,
   Text,
+  Card,
+  CardBody,
+  CardHeader, 
+  CardFooter,
+  Stack,
+  Divider,
+  Tooltip,
+  ButtonGroup,
 } from '@chakra-ui/react';
+import { HamburgerIcon } from "@chakra-ui/icons"
 import { Alchemy, Network } from 'alchemy-sdk';
 import { useState } from 'react';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useAccount } from 'wagmi';
+import { InfinitySpin } from 'react-loader-spinner';
+
 
 function App() {
   const [userAddress, setUserAddress] = useState('');
   const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [hasQueried, setHasQueried] = useState(false);
   const [tokenDataObjects, setTokenDataObjects] = useState([]);
+  const [nftData, setNftData] = useState([]);
+
+  useAccount({
+    onConnect({ address, connector, isReconnected }) {
+      console.log('Connected', { address, connector, isReconnected })
+      setUserAddress(address)
+    },
+  })
 
   async function getNFTsForOwner() {
+    setIsLoading(true);
     const config = {
-      apiKey: '<-- COPY-PASTE YOUR ALCHEMY API KEY HERE -->',
+      apiKey: import.meta.env.API_KEY,
       network: Network.ETH_MAINNET,
     };
 
     const alchemy = new Alchemy(config);
     const data = await alchemy.nft.getNftsForOwner(userAddress);
-    setResults(data);
-
-    const tokenDataPromises = [];
-
-    for (let i = 0; i < data.ownedNfts.length; i++) {
-      const tokenData = alchemy.nft.getNftMetadata(
-        data.ownedNfts[i].contract.address,
-        data.ownedNfts[i].tokenId
-      );
-      tokenDataPromises.push(tokenData);
-    }
-
-    setTokenDataObjects(await Promise.all(tokenDataPromises));
+    console.log("initial nft data:", data);
+    setIsLoading(false);
     setHasQueried(true);
+    setNftData(data);
   }
   return (
     <Box w="100vw">
       <Center>
+      <ConnectButton />
         <Flex
           alignItems={'center'}
           justifyContent="center"
@@ -67,49 +81,66 @@ function App() {
         <Input
           onChange={(e) => setUserAddress(e.target.value)}
           color="black"
+          value={userAddress}
           w="600px"
           textAlign="center"
           p={4}
           bgColor="white"
           fontSize={24}
         />
-        <Button fontSize={20} onClick={getNFTsForOwner} mt={36} bgColor="blue">
+        <Button fontSize={20} onClick={getNFTsForOwner} mt={36} bgColor="#0098fd">
           Fetch NFTs
         </Button>
+        {(!isLoading && hasQueried) && <div className='margin-b'>
+        <Divider/>
 
-        <Heading my={36}>Here are your NFTs:</Heading>
-
-        {hasQueried ? (
-          <SimpleGrid w={'90vw'} columns={4} spacing={24}>
-            {results.ownedNfts.map((e, i) => {
-              return (
-                <Flex
-                  flexDir={'column'}
-                  color="white"
-                  bg="blue"
-                  w={'20vw'}
-                  key={e.id}
-                >
-                  <Box>
-                    <b>Name:</b>{' '}
-                    {tokenDataObjects[i].title?.length === 0
-                      ? 'No Name'
-                      : tokenDataObjects[i].title}
-                  </Box>
-                  <Image
-                    src={
-                      tokenDataObjects[i]?.rawMetadata?.image ??
-                      'https://via.placeholder.com/200'
-                    }
-                    alt={'Image'}
-                  />
-                </Flex>
-              );
-            })}
-          </SimpleGrid>
-        ) : (
-          'Please make a query! The query may take a few seconds...'
-        )}
+        <Heading className='title-main' my={36}>You have <strong className='nft-count'> {" "+ nftData.ownedNfts.length+ " "} </strong> NFTs in total</Heading>
+        <div className='flex-cards'>
+          {nftData.ownedNfts.map(nft=>{
+            console.log(nft)
+            return (<Card className='card' maxW={"sm"}>
+              <CardBody>
+                <Image className='small-img' src={nft.rawMetadata?.image ?? 'https://via.placeholder.com/200'}/>
+              
+              <Stack mt="6" spacing="3">
+              <Heading color={"blue"} textDecoration={"underline"} size="md"><a target='_blank' href={`https://opensea.io/assets/ethereum/${nft.contract.address}/${nft.tokenId}`}>{nft.contract?.name ?? "Untitled"} #{nft.tokenId.length > 10 ? nft.tokenId.slice(0,4)+"..."+nft.tokenId.slice(nft.tokenId.length-4,nft.tokenId.length) : nft.tokenId}</a></Heading>
+                <Text>Creator/Deployer Address: <a target='_blank' href={`https://etherscan.io/address/${nft.contract.contractDeployer}`}>{nft.contract.contractDeployer.slice(0,5)+"..."+nft.contract.contractDeployer.slice(nft.contract.contractDeployer.length-5,nft.contract.contractDeployer.length)}</a></Text>
+                <Divider/>
+                <Text>
+                  {nft.contract.openSea?.description?.length > 205 ? <div>{nft.contract.openSea.description.slice(0,205)+" "}<Tooltip  label={nft.contract.openSea.description.slice(205,nft.contract.openSea.description.length)}> [...] </Tooltip></div>:nft.contract.openSea?.description  ?? "No description is available for this collection"}
+                </Text>
+                <Divider/>
+                <div className='flex-r'>
+                <Text>Symbol</Text>
+                <Text>Total Supply</Text>
+                <Text>Floor Price</Text>
+                </div>
+                
+                <div className='flex-r'>
+                <Text color={"blue"}><a target='_blank' href={`https://etherscan.io/token/${nft.contract.address}`}>{nft.contract?.symbol}</a></Text>
+                <Text>{nft.contract.totalSupply}</Text>
+                <Text fontWeight={"bold"}>{nft.contract.openSea?.floorPrice < 0.001 ? "~0 ": nft.contract.openSea.floorPrice  ?? "0"} ETH</Text>
+                </div>
+              </Stack>
+              </CardBody>
+              <Divider/>
+              <CardFooter className='flexing'>
+              <div className='flex-c'>
+              <Text>Website</Text>
+              <Button><a target='_blank' href={nft.contract.openSea.externalUrl}><i class="fa-solid fa-globe"></i></a></Button>
+              </div>
+              <Divider colorScheme='black' className='vertical' orientation='vertical' />
+              <div className='flex-c'>
+              <Text>OpenSea</Text>
+              <Button><a target='_blank' href={`https://opensea.io/assets/ethereum/${nft.contract.address}`}><i class="fa-solid fa-store"></i></a></Button>
+              </div>
+              </CardFooter>
+            </Card>)
+          })
+          }
+          </div>
+          </div>
+          }
       </Flex>
     </Box>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './index.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  zora,
+} from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import { ChakraProvider } from '@chakra-ui/react'
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, zora],
+  [
+    alchemyProvider({ apiKey: import.meta.env.VITE_API_KEY }),
+    publicProvider()  
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'NFT Indexer dApp',
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  chains
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ChakraProvider>
+  <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
+    </ChakraProvider>,
 )
